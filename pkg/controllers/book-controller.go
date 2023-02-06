1c1d5a48f5255bcfc6ab6ba6c6a6af58c8bf2b0e
greatchinex <greatchinex@gmail.com>
2023-02-06 10:01:05 +0100

    Added controller methods

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,21 +1,59 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	"github.com/Greatchinex/bookstore/pkg/models"
+	"github.com/Greatchinex/bookstore/pkg/utils"
 )
+
+type ResponseStatus struct {
+	Status bool	`json:"status"`
+	Message string `json:"message"`
+}
 
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create Book Method")
+	newBook := models.Book{}
+	if err := utils.DecodeJson(r, &newBook); err != nil {
+		fmt.Println(err)
+	}
+
+	book := newBook.CreateBook()
+	res, _ := json.Marshal(book)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get Multiple Books Method")
+	allBooks := models.GetAllBooks()
+	res, _ := json.Marshal(allBooks)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetBookById(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Get Single Book Method")
+	Id := mux.Vars(r)["bookId"]
+	ID, err := strconv.ParseInt(Id, 0, 0)
+	if err != nil {
+		fmt.Printf("Error converting string %v to integer\n", ID)
+	}
+
+	singleBook := models.GetBookById(ID)
+	res, _ := json.Marshal(singleBook)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +61,17 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Delete Single Book Method")
+	params := mux.Vars(r)["bookId"]
+	Id, err := strconv.ParseInt(params, 0, 0)
+	if err != nil {
+		fmt.Printf("Error converting string %v to integer\n", Id)
+	}
+
+	models.DeleteBook(Id)
+	apiResponse := ResponseStatus{Status: true, Message: "Book deleted successfully"}
+	res, _ := json.Marshal(apiResponse)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+// File contains two methods that decodes json input to a golang type, Both were added just to see the different ways
+// it can be done. It is recommended to use json.decode if your payload is coming from an io.Reader(e.g http request body)
+// and use unmarshall if you already have the JSON data in memory(like a variable).
+/**
+https://stackoverflow.com/questions/21197239/decoding-json-using-json-unmarshal-vs-json-newdecoder-decode
+https://stackoverflow.com/questions/15672556/handling-json-post-request-in-go
+*/
+
+func DecodeJson(r *http.Request, x interface{}) error {
+	return json.NewDecoder(r.Body).Decode(x)
+}
+
 // Convert incoming request payload from json to a datatype golang can use
 // Method takes in request object and an "any" type of request body
 func ConvertFromJson(r *http.Request, x interface{}) {
